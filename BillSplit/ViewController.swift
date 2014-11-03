a816283722e9f7ff15be9dd2546e6c9b2ee34300
Anthony Picciano <anthony.picciano@gmail.com>
2014-11-03 14:46:07 -0700

    minor change

--- BillSplit/ViewController.swift
+++ BillSplit/ViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalAmountField: UITextField!
+    @IBOutlet weak var numberOfSplitsLabel: UILabel!
+    @IBOutlet weak var splitAmountLabel: UILabel!
+    @IBOutlet weak var splitStepper: UIStepper!
+    @IBOutlet weak var tipPercentageSlider: UISlider!
+    
+    var totalAmount = 10.0
+    let formatter = NSNumberFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        formatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        formatter.locale = NSLocale(localeIdentifier: "en_US")
+        
+        updateDisplay()
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        totalAmountField.resignFirstResponder()
+        updateDisplay()
+    }
+    
+    func updateDisplay() {
+        totalAmountField.text = "\(formatCurrency(totalAmount))"
+        numberOfSplitsLabel.text = "\(Int(splitStepper.value))"
+        
+        let splitAmount = totalAmount * Double(tipPercentageSlider.value) / splitStepper.value
+        splitAmountLabel.text = "\(formatCurrency(splitAmount))"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func stepperChanged(sender: AnyObject) {
+        updateDisplay()
     }
-
-
+    
+    func formatCurrency(value:Double) -> String {
+        return formatter.stringFromNumber(value)!
+    }
+    
+    @IBAction func valueChanged(sender: AnyObject) {
+        let field = sender as UITextField
+        if let number = formatter.numberFromString(field.text) {
+            totalAmount = number.doubleValue
+        }
+    }
+    
+    @IBAction func tipPercentageChanged(sender: AnyObject) {
+        updateDisplay()
+    }
 }
 
